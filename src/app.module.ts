fb2f405afbad3a7197cea07e6e359d5d50bd644d
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2024-06-12 23:44:03 -0300

    refactor: modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, CategoriesModule, OrdersModule, ProductsModule],
 })
 export class AppModule {}
