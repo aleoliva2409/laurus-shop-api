9221b3225f938401165b2bec341b8978a295f771
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2024-07-01 21:35:05 -0300

    feat: response interceptor added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
+// import { ResponseInterceptor } from './shared/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // app.useGlobalInterceptors(new ResponseInterceptor());
+
+  await app.listen(3001);
 }
 bootstrap();
