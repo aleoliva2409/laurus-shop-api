bad9256faa90d8fecce8d83879b8338d3f302718
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2024-07-09 00:44:32 -0300

    feat: swagger installed and configured

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // import { ResponseInterceptor } from './shared/interceptors';
 
@@ -9,6 +10,15 @@ async function bootstrap() {
 
   // app.useGlobalInterceptors(new ResponseInterceptor());
 
-  await app.listen(3001);
+  const config = new DocumentBuilder()
+    .setTitle('Laurus shop API documentation')
+    .setDescription('All endpoints available to test')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
